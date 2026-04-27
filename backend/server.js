822866be050b32dc491f93bcb191d7215165b83c
amipatel0 <amipatel081103@gmail.com>
2026-04-27 21:52:09 +0530

    commit 27/4

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 connectDB();
-
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/customers', customerRoutes);
 app.use('/api/customers/:customerId/payments', paymentRoutes);
